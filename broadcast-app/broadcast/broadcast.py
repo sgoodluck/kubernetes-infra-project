5676fdce6dd721edafd72a1687f7efd151389399
Seth <smartin@schmidtfutures.com>
2024-07-13 14:28:39 +0200

    fix: updates to use version 2 api for mqtt
    Apparently we need to include callback functions now - so added

--- broadcast-app/broadcast/broadcast.py
+++ broadcast-app/broadcast/broadcast.py
@@ -1,16 +1,28 @@
-import paho.mqtt.client as mqtt
+from paho.mqtt import client as mqtt_client
 import time
 import random
 
-broker_address = "mosquitto-broker"
+def on_connect(client, userdata, flags, rc, properties=None):
+    print(f"Connected with result code {rc}")
+
+def on_publish(client, userdata, mid, properties=None):
+    print(f"Message {mid} published")
+
+broker_address = "test.mosquitto.org"
 topic = "hello_world/broadcast"
-
-client = mqtt.Client("Broadcast", protocol=mqtt.MQTTv5)
-client.connect(broker_address)
-
 message = "Hello world"
+
+client = mqtt_client.Client(client_id="Broadcast", protocol=mqtt_client.MQTTv5)
+client.on_connect = on_connect
+client.on_publish = on_publish
+
+client.connect(broker_address)
+client.loop_start()
 
 while True:
     wait_time = random.uniform(1, 10)
     time.sleep(wait_time)
-    client.publish(topic, message)
+    result = client.publish(topic, message)
+    result.wait_for_publish()
+
+client.loop_stop()
