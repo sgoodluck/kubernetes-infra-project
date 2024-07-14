d7d69806d6b2014f0ec5bcc2d8cd0c4df565c6df
Seth <smartin@schmidtfutures.com>
2024-07-15 01:10:08 +0200

    feature: improves deployment script
    Uses some nice colors and prints the URL for convenience

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Function to check if image exists in Minikube
+
+# helper to check if the image exists
 check_image() {
   image_name=$1
   if minikube ssh "docker image inspect $image_name" &> /dev/null; then
@@ -10,6 +11,13 @@ check_image() {
     exit 1
   fi
 }
+
+# Set some colors
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+
+# Let everyone know
+echo -e "\n${GREEN}Starting Deployment Process...${CLEAR}\n"
 
 # Delete existing deployments if they exist
 kubectl delete deployment broker-app receive-app broadcast-app
@@ -30,3 +38,22 @@ kubectl rollout status deployment/broker-app
 kubectl apply -f kubernetes/broadcast-app-deployment.yml
 kubectl apply -f kubernetes/receive-app-deployment.yml
 kubectl apply -f kubernetes/receive-app-service.yml
+
+# Wait for receive-app deployment
+kubectl rollout status deployment/receive-app
+
+# Start port forwarding in the background and redirect output to null
+echo -e "\nStarting port-forwarding..."
+kubectl port-forward service/receive-app-service 5000:80 > /dev/null 2>&1 &
+
+# Print out url
+echo -e "\n${GREEN}Flask app is accessible at: http://localhost:5000\n${CLEAR}"
+
+# Await user input to kill process
+read -p $'Press Enter to stop port forwarding and exit...'
+
+# Kill the port forwarding process
+pkill -f "kubectl port-forward"
+
+# Bye Now
+echo -e "\n${GREEN}Exiting!${CLEAR}"
