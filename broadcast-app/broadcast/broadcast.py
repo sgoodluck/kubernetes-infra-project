5b6ddca7f2351bedec70230ebc68d1bb69d2450d
Seth <smartin@schmidtfutures.com>
2024-07-15 10:55:35 +0200

    cleans up broadcast-app files

--- broadcast-app/broadcast/broadcast.py
+++ broadcast-app/broadcast/broadcast.py
@@ -3,19 +3,23 @@ import time
 import random
 import logging
 
+# Init logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Define mqtt constants
+BROKeR_ADDRESS = "broker-app"
+BROKER_PORT = 1883
+TOPIC = "hello_world/broadcast"
+MESSAGE = "Hello world"
+
+# Setup functions for mqtt
 def on_connect(client, userdata, flags, rc, properties=None):
-    logging.info(f"Connected with result code {rc}")
+    logging.info(f"Connected to broker with result code {rc}")
 
 def on_publish(client, userdata, mid, properties=None, reasonCode=None):
     logging.info(f"Message {mid} published")
 
-broker_address = "broker-app"
-broker_port = 1883
-topic = "hello_world/broadcast"
-message = "Hello world"
-
+# Configure mqtt
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_publish = on_publish
@@ -23,7 +27,6 @@ client.protocol_version = mqtt.MQTTv5
 
 try:
     client.connect(broker_address, broker_port)
-    logging.info("Connected to MQTT broker")
     client.loop_start()
 
     while True:
