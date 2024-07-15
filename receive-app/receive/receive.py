400bdaeeda5f1667f1ebd662ac2c3d1c1aee9fcc
Seth <smartin@schmidtfutures.com>
2024-07-15 13:58:06 +0200

    implements live load of messages on browser
    Also cleans up some artifacts

--- receive-app/receive/receive.py
+++ receive-app/receive/receive.py
@@ -1,18 +1,22 @@
 import paho.mqtt.client as mqtt
 import logging
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 # Init logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Init flask app and array to store messages
-app = Flask(__name__)
-received_messages = []
 
 # Init Settings
 BROKER_ADDRESS = "broker-app"
 BROKER_PORT = 1883
 TOPIC = "hello_world/broadcast"
+
+# Init flask app, SocketIO, and array to store messages
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+received_messages = []
 
 # Init mqtt callback functions
 def on_connect(client, userdata, flags, rc, properties=None):
@@ -20,16 +24,16 @@ def on_connect(client, userdata, flags, rc, properties=None):
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, message):
-    logging.info("Received message: " + str(message.payload.decode("utf-8")))
-    received_messages.append(str(message.payload.decode("utf-8")))
-
+    decoded_message = str(message.payload.decode("utf-8"))
+    logging.info(f"Received message: {decoded_message}")
+    received_messages.append(decoded_message)
+    socketio.emit('new_message', {'message': decoded_message}, namespace='/')
 
 # Configure mqtt
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 client.protocol_version = mqtt.MQTTv5
-
 
 try:
     client.connect(BROKER_ADDRESS, BROKER_PORT)
@@ -40,9 +44,9 @@ try:
     def index():
         return render_template('index.html', messages=received_messages)
 
-    # Run flask app on localhost
+    # Run flask app with SocketIO and allow unsafe for non-production
     if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
 
 except KeyboardInterrupt:
     logging.info("Stopping the receiver...")
@@ -51,6 +55,6 @@ except Exception as e:
     logging.error(f"Exception occurred: {str(e)}")
 
 finally:
-    client.loop_stop() 
+    client.loop_stop()
     client.disconnect()
     logging.info("Disconnected from MQTT broker")
